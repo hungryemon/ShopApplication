55bd65e847f27540121343228ccd22919d69d1f4
hungryemon <emonshahed@gmail.com>
2020-02-20 22:24:58 +0600

    Added provider class for products.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,18 +11,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return MaterialApp(
-      title: 'Walmart',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        accentColor:  Colors.tealAccent,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      builder: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'Walmart',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          accentColor:  Colors.redAccent,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+          routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen()
+          }
       ),
-      home: ProductsOverviewScreen(),
-        routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen()
-        }
     );
   }
 
